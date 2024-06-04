d25e016a3ce6e71265b39ba61646dca5a2a9d975
leehyukshuai <leehyukshuai@stu.pku.edu.cn>
2024-06-04 21:48:58 +0800

    简单重构了下object实现

--- src/VCX/Labs/4-OpenProj/Object.cpp
+++ src/VCX/Labs/4-OpenProj/Object.cpp
@@ -1,5 +1,30 @@
 #include "Object.h"
 
 namespace VCX::Labs::OpenProj {
+    Object::Object(RigidBody * bodyPtr):
+        rigidBody(bodyPtr) {}
 
-}
+    Box::Box(glm::vec3 dim):
+        Object(&boxBody) {
+        initialize(dim);
+    }
+
+    void Box::initialize(const glm::vec3 & dim) {
+        auto mesh = Mesh::generateBoxMesh(dim);
+        renderItem.initialize(mesh);
+        auto geom = std::make_shared<fcl::Box<float>>(dim[0], dim[1], dim[2]);
+        collisionItem.initialize(geom);
+    }
+
+    Cylinder::Cylinder(float radius, float height, int precision):
+        Object(&cylinderBody) {
+        initialize(radius, height, precision);
+    }
+
+    void Cylinder::initialize(float radius, float height, int precision) {
+        auto mesh = Mesh::generateCylinderMesh(radius, height, precision);
+        renderItem.initialize(mesh);
+        auto geom = std::make_shared<fcl::Cylinder<float>>(radius, height);
+        collisionItem.initialize(geom);
+    }
+} // namespace VCX::Labs::OpenProj
--- src/VCX/Labs/4-OpenProj/Object.h
+++ src/VCX/Labs/4-OpenProj/Object.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "Collision.h"
 #include "Render.h"
 #include "RigidBody.h"
+#include "Collision.h"
 
 namespace VCX::Labs::OpenProj {
     class Object {
@@ -19,34 +19,16 @@ namespace VCX::Labs::OpenProj {
     public:
         BoxBody boxBody;
 
-        Box(glm::vec3 dim = glm::vec3(1, 1, 1)):
-            Object(&boxBody) {
-            initialize(dim);
-        }
-
-        void initialize(const glm::vec3 & dim) {
-            auto mesh = Mesh::generateBoxMesh(dim);
-            renderItem.initialize(mesh);
-            auto geom = std::make_shared<fcl::Box<float>>(dim[0], dim[1], dim[2]);
-            collisionItem.initialize(geom);
-        }
+        Box(glm::vec3 dim = glm::vec3(1, 1, 1));
+        void initialize(const glm::vec3 & dim);
     };
 
     class Cylinder : public Object {
     public:
         CylinderBody cylinderBody;
 
-        Cylinder(float radius = 0.5f, float height = 1.0f, int precision = 32):
-            Object(&cylinderBody) {
-            initialize(radius, height, precision);
-        }
-
-        void initialize(float radius, float height, int precision) {
-            auto mesh = Mesh::generateCylinderMesh(radius, height, precision);
-            renderItem.initialize(mesh);
-            auto geom = std::make_shared<fcl::Cylinder<float>>(radius, height);
-            collisionItem.initialize(geom);
-        }
+        Cylinder(float radius = 0.5f, float height = 1.0f, int precision = 32);
+        void initialize(float radius, float height, int precision);
     };
 
 } // namespace VCX::Labs::OpenProj
